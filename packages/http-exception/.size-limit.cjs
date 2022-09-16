d854d921b92751fecce8cd4f2038f4c9d94dacd9
Sébastien Vanvelthem <belgattitude@gmail.com>
2022-09-16 13:24:21 +0200

    chore(size-limit): increase to account for legacy browsers

--- packages/http-exception/.size-limit.cjs
+++ packages/http-exception/.size-limit.cjs
@@ -1,7 +1,7 @@
 // @ts-check
 
-// Regularly update this max size when new legit code is added (keep a threshold)
-const fullBundleMaxSize = "2150B";
+const fullEsmMaxSize = "2900B";
+const fullCjsMaxSize = "3100B";
 
 /**
  * Will ensure esm tree-shakeability and total size are within expectations.
@@ -17,46 +17,53 @@ module.exports = [
     name: "ESM (import everything *)",
     path: ["dist/esm/index.js"],
     import: "*",
-    limit: fullBundleMaxSize,
+    limit: fullEsmMaxSize,
   },
   {
     name: "ESM (only HttpNotFound exception)",
     path: ["dist/esm/index.js"],
     import: "{ HttpNotFound }",
-    limit: "360B",
+    limit: "1100B",
   },
   {
     name: "ESM (only HttpInternalServerError)",
     path: ["dist/esm/index.js"],
     import: "{ HttpInternalServerError }",
-    limit: "370B",
+    limit: "1100B",
   },
   {
     name: "ESM (two exceptions: HttpNotFound + HttpInternalServerError)",
     path: ["dist/esm/index.js"],
     import: "{ HttpNotFound, HttpInternalServerError }",
-    limit: "420B",
+    limit: "1200B",
   },
   {
     name: "ESM (only isHttpException)",
     path: ["dist/esm/index.js"],
     import: "{ isHttpException }",
-    limit: "290B",
+    limit: "1000B",
   },
   {
     name: "ESM (only createHttpException)",
     path: ["dist/esm/index.js"],
     import: "{ createHttpException }",
-    limit: "1750B", // Will import all server/client exceptions
+    limit: "2400B", // Will import all server/client exceptions
   },
   // ###################################################
   // Commonjs full bundle
   // ###################################################
   {
     name: "CJS (require everything *)",
-    path: ["dist/cjs/index.js"],
+    path: ["dist/cjs/index.cjs"],
     import: "*",
     webpack: true,
-    limit: fullBundleMaxSize,
+    limit: fullCjsMaxSize,
+  },
+  {
+    name: "CJS (only isHttpException)",
+    path: ["dist/cjs/index.cjs"],
+    import: "{ isHttpException }",
+    webpack: true,
+    limit: '2200B',
   }
 ];
