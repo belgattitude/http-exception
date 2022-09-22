d31bfd76b38cdf671f376f226fa2237267518cfa
Sébastien Vanvelthem <belgattitude@gmail.com>
2022-09-22 23:19:20 +0200

    feat: updated example

--- examples/nextjs-app/src/pages/api/status/[statusCode].tsx
+++ examples/nextjs-app/src/pages/api/status/[statusCode].tsx
@@ -5,21 +5,45 @@ import {
 } from '@belgattitude/http-exception';
 import { toJson } from '@belgattitude/http-exception/serializer';
 import type { NextApiRequest, NextApiResponse } from 'next';
+import type { ZodTypeAny } from 'zod';
+import { z } from 'zod';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { statusCode = null } = req.query;
+export const statusCode = (schema: ZodTypeAny) =>
+  z.preprocess(
+    (a) => (typeof a === 'string' ? parseInt(a, 10) : undefined),
+    schema
+  );
 
-  const status =
-    typeof statusCode === 'string'
-      ? Number.parseInt(statusCode, 10)
-      : HttpBadRequest.STATUS;
+const reqSchema = z.object({
+  query: z.object({
+    statusCode: statusCode(z.number().min(100).max(599)),
+  }),
+});
+
+export default function statusCodehandler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const parsed = reqSchema.safeParse(req, {});
+  const status = parsed.success
+    ? parsed.data.query.statusCode
+    : HttpBadRequest.STATUS;
 
   if (isHttpErrorStatusCode(status)) {
-    const e = createHttpException(status);
+    const e = createHttpException(status, {
+      url: req.headers['host'],
+    });
     res.status(status).json({
       success: false,
-      error: toJson(e),
+      error: {
+        status: e.statusCode,
+        name: e.name,
+        message: e.message,
+        url: e.url,
+        serialized: JSON.parse(toJson(e)),
+      },
     });
+    return;
   }
 
   res.status(status).json({
